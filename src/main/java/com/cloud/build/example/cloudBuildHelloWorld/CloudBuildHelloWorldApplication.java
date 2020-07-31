a964af63bdfd8ec3e77e6ce5e9df7e2bac0ca326
Vishnuviki <vishnuvenugopalp1@gmail.com>
2020-07-31 19:47:41 +0530

    first commit

---
+++ src/main/java/com/cloud/build/example/cloudBuildHelloWorld/CloudBuildHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.build.example.cloudBuildHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBuildHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBuildHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/build/example/cloudBuildHelloWorld/CloudBuildHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.build.example.cloudBuildHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBuildHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
